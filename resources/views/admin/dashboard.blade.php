cfb60e844b4875fefe1143c99be1212bab3c8ce4
zeeshan-tai <zeeshantai786@gmail.com>
2023-08-18 17:25:06 +0500

    create resource controller

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,72 +1,84 @@
-@extends('admin.layout.master')  
+  @extends('admin.layout.master')
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
 
-@section('main-content')
-
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>Dashboard</h1>
-  </section>
-
-  <!-- Main content -->
-  <section class="content">
-
-    <div class="row">
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-aqua"><i class="fa fa-tasks"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Categoreis</span>
-            <span class="info-box-number">1,410</span>
-          </div>
-          <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-green"><i class="fa fa-pencil"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Authors</span>
-            <span class="info-box-number">410</span>
-          </div>
-          <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-yellow"><i class="fa fa-book"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Books</span>
-            <span class="info-box-number">13,648</span>
-          </div>
-          <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-red"><i class="fa fa-users"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Teams</span>
-            <span class="info-box-number">93,139</span>
-          </div>
-          <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
-</section>
-
-@endsection
   
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>Dashboard</h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-tasks"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Categoreis</span>
+              <span class="info-box-number">1,410</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-pencil"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Authors</span>
+              <span class="info-box-number">410</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-book"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Books</span>
+              <span class="info-box-number">13,648</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-users"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Teams</span>
+              <span class="info-box-number">93,139</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+  </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+ 
+<!-- ./wrapper -->
+
+<script src="assets/admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="assets/admin/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="assets/admin/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="assets/admin/dist/js/app.min.js"></script>
+<script src="assets/admin/dist/js/demo.js"></script>
