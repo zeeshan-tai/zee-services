35dcc2f37dbcad9449edbeca6753c3ea9bc7ba0d
zeeshan-tai <zeeshantai786@gmail.com>
2023-08-19 15:16:26 +0500

    work submit

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -5,11 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Home | American Books</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="assets/admin/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{asset('assets/admin/bootstrap/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <link rel="stylesheet" href="assets/admin/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="assets/admin/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/admin/dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{asset('assets/admin/dist/css/skins/_all-skins.min.css')}}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,12 +35,12 @@
  
 <!-- ./wrapper -->
 
-<script src="assets/admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<script src="assets/admin/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{asset('assets/admin/plugins/jQuery/jquery-2.2.3.min.js')}}"></script>
+<script src="{{asset('assets/admin/bootstrap/js/bootstrap.min.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="assets/admin/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<script src="assets/admin/dist/js/app.min.js"></script>
-<script src="assets/admin/dist/js/demo.js"></script>
+<script src="{{asset('assets/admin/plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('assets/admin/dist/js/app.min.j')}}s"></script>
+<script src="{{asset('assets/admin/dist/js/demo.js')}}"></script>
 
 </body>
 </html>
