9a48c3e8d92893e180169e5dcf368919f23c98cb
shahmeer <zeeshantai786@gmail.com>
2023-08-26 16:18:16 +0500

    done

--- app/Http/Controllers/Front/ProjectController.php
+++ app/Http/Controllers/Front/ProjectController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\project;
 
 class ProjectController extends Controller
 {
     public function index()
     {
+        $projects = Project::latest()->get();
         $page_title = "Our Projects";
-        return view('front.project', compact('page_title'));
+        return view('front.project', compact('page_title', 'projects'));
     }
 }
