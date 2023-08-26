38ec46855d691e1057396493e6b00543acff0957
Hafiz-mohsin <hafizmohsin400@gmail.com>
2023-08-26 16:29:43 +0500

    done

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Project;
 use Illuminate\Http\Request;
 use App\Service;
-
 class HomeController extends Controller
 {
     public function index()
     {
+        $projects= Project::latest()->get();
         $services = Service::latest()->get();
-        return view('front.index', compact('services'));
+        return view('front.index', compact('services', 'projects'));
     }
 }
